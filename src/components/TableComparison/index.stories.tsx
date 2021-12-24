2e0ce3c8706c52ef189883f328c34d9e9a0b0e5c
SULTAN PEYEK <sultanpeyek999@gmail.com>
2021-12-24 13:47:19 +0700

    Finish TableComparison component

--- src/components/TableComparison/index.stories.tsx
+++ src/components/TableComparison/index.stories.tsx
@@ -4,12 +4,17 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { TableComparison } from './index';
 
 // Import them by yourself
-import { Container, Grid, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import WorkIcon from '@mui/icons-material/Work';
 import LightbulbIcon from '@mui/icons-material/Lightbulb';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import RemoveIcon from '@mui/icons-material/Remove';
 import CheckIcon from '@mui/icons-material/Check';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import LoginIcon from '@mui/icons-material/Login';
+import ParaglidingIcon from '@mui/icons-material/Paragliding';
+import GroupWorkIcon from '@mui/icons-material/GroupWork';
 
 export default {
   title: 'Grape/TableComparison',
@@ -17,102 +22,169 @@ export default {
 } as ComponentMeta<typeof TableComparison>;
 
 const data = {
-  gibbon: {
-    className: 'C',
-    classAlias: 'Gibbon',
-    minToken: '1,020',
-    minTokenLP: '2,000',
-    value: [
-      <React.Fragment>
-        <LightbulbIcon sx={{ width: 50, height: 50 }} />
+  headings: [
+    <Typography
+      sx={{
+        alignSelf: 'flex-start',
+        fontSize: '2rem',
+      }}
+    >
+      Membership Models
+    </Typography>,
+    <Typography
+      sx={{
+        fontSize: '1.2rem',
+      }}
+    >
+      <CardGiftcardIcon
+        sx={{ marginBottom: '0.125rem', marginRight: '0.8rem', verticalAlign: 'middle' }}
+      />
+      Monthly Emissions
+    </Typography>,
+    <Typography
+      sx={{
+        fontSize: '1.2rem',
+      }}
+    >
+      <EmojiEventsIcon
+        sx={{ marginBottom: '0.125rem', marginRight: '0.8rem', verticalAlign: 'middle' }}
+      />
+      Event Prize Pools
+    </Typography>,
+    <Typography
+      sx={{
+        fontSize: '1.2rem',
+      }}
+    >
+      <WorkIcon sx={{ marginBottom: '0.125rem', marginRight: '0.8rem', verticalAlign: 'middle' }} />
+      Access to Skill Role Allocation Pool
+    </Typography>,
+    <Typography
+      sx={{
+        fontSize: '1.2rem',
+      }}
+    >
+      <LoginIcon
+        sx={{ marginBottom: '0.125rem', marginRight: '0.8rem', verticalAlign: 'middle' }}
+      />
+      Channel Access Level
+    </Typography>,
+    <Typography
+      sx={{
+        fontSize: '1.2rem',
+      }}
+    >
+      <ParaglidingIcon
+        sx={{ marginBottom: '0.125rem', marginRight: '0.8rem', verticalAlign: 'middle' }}
+      />
+      Chance to win Airdrops / Giveaways
+    </Typography>,
+    <Typography
+      sx={{
+        fontSize: '1.2rem',
+      }}
+    >
+      <GroupWorkIcon
+        sx={{ marginBottom: '0.125rem', marginRight: '0.8rem', verticalAlign: 'middle' }}
+      />
+      Governance: SubDAO Eligibility
+    </Typography>,
+    <Typography
+      sx={{
+        fontSize: '1.2rem',
+      }}
+    >
+      <PeopleAltIcon
+        sx={{ marginBottom: '0.125rem', marginRight: '0.8rem', verticalAlign: 'middle' }}
+      />
+      Governance: MainDAO Eligibility
+    </Typography>,
+  ],
+  items: [
+    {
+      className: 'C',
+      classAlias: 'Gibbon',
+      minToken: '1,020',
+      minTokenLP: '(2,000 Raydium LP)',
+      values: [
+        <React.Fragment>
+          <LightbulbIcon sx={{ width: 50, height: 50 }} />
+          <Typography>
+            Community &amp; Giveaways: Participate in contests, airdrops, giveaways, beta testing to
+            win upto <strong>100K $GRAPE/month+</strong> partner giveaways.
+          </Typography>
+        </React.Fragment>,
+        <Typography>10 $GRAPE</Typography>,
+        <Typography>150,000 $GRAPE</Typography>,
         <Typography>
-          Community &amp; Giveaways: Participate in contests, airdrops, giveaways, beta testing to
-          win upto <strong>100K $GRAPE/month+</strong> partner giveaways.
-        </Typography>
-      </React.Fragment>,
-      <Typography>10 $GRAPE</Typography>,
-      <Typography>150,000 $GRAPE</Typography>,
-      <Typography>
-        <RemoveIcon />
-      </Typography>,
-      <Typography>65%</Typography>,
-      <Typography>1x</Typography>,
-      <Typography>
-        <RemoveIcon />
-      </Typography>,
-      <Typography>
-        <RemoveIcon />
-      </Typography>,
-    ],
-  },
-  greatApe: {
-    className: 'B',
-    classAlias: 'Great Ape',
-    minToken: '5,100',
-    minTokenLP: '10,000',
-    value: [
-      <React.Fragment>
-        <WorkIcon sx={{ width: 50, height: 50 }} />
+          <RemoveIcon />
+        </Typography>,
+        <Typography>65%</Typography>,
+        <Typography>1x</Typography>,
         <Typography>
-          Work &amp; Earn: Show off your skills &amp; earn from a pool of{' '}
-          <strong>1.5mn Grape/month</strong> by fulfilling work-tickets or specific bounties from
-          SOL founders. Access to skill group sub-DAOs
-        </Typography>
-      </React.Fragment>,
-      <Typography>50 $GRAPE</Typography>,
-      <Typography>250,000 $GRAPE</Typography>,
-      <Typography>1,500,000 $GRAPE</Typography>,
-      <Typography>85%</Typography>,
-      <Typography>5x</Typography>,
-      <Typography>
-        <CheckIcon />
-      </Typography>,
-      <Typography>
-        <RemoveIcon />
-      </Typography>,
-    ],
-  },
-  gorilla: {
-    className: 'A',
-    classAlias: 'Gorilla',
-    minToken: '20,400',
-    minTokenLP: '40,000',
-    value: [
-      <React.Fragment>
-        <PeopleAltIcon sx={{ width: 50, height: 50 }} />
+          <RemoveIcon />
+        </Typography>,
         <Typography>
-          Governance: Apply to be a part of the DAO and shape our roadmap, capital allocation and
-          growth within the Solana ecosystem
-        </Typography>
-      </React.Fragment>,
-      <Typography>200 $GRAPE</Typography>,
-      <Typography>500,000 $GRAPE</Typography>,
-      <Typography>1,500,000 $GRAPE</Typography>,
-      <Typography>100%</Typography>,
-      <Typography>20x</Typography>,
-      <Typography>
-        <CheckIcon />
-      </Typography>,
-      <Typography>
-        <CheckIcon />
-      </Typography>,
-    ],
-  },
+          <RemoveIcon />
+        </Typography>,
+      ],
+    },
+    {
+      className: 'B',
+      classAlias: 'Great Ape',
+      minToken: '5,100',
+      minTokenLP: '(10,000 Raydium LP)',
+      values: [
+        <React.Fragment>
+          <WorkIcon sx={{ width: 50, height: 50 }} />
+          <Typography>
+            Work &amp; Earn: Show off your skills &amp; earn from a pool of{' '}
+            <strong>1.5mn Grape/month</strong> by fulfilling work-tickets or specific bounties from
+            SOL founders. Access to skill group sub-DAOs
+          </Typography>
+        </React.Fragment>,
+        <Typography>50 $GRAPE</Typography>,
+        <Typography>250,000 $GRAPE</Typography>,
+        <Typography>1,500,000 $GRAPE</Typography>,
+        <Typography>85%</Typography>,
+        <Typography>5x</Typography>,
+        <Typography>
+          <CheckIcon />
+        </Typography>,
+        <Typography>
+          <RemoveIcon />
+        </Typography>,
+      ],
+    },
+    {
+      className: 'A',
+      classAlias: 'Gorilla',
+      minToken: '20,400',
+      minTokenLP: '(40,000 Raydium LP)',
+      values: [
+        <React.Fragment>
+          <PeopleAltIcon sx={{ width: 50, height: 50 }} />
+          <Typography>
+            Governance: Apply to be a part of the DAO and shape our roadmap, capital allocation and
+            growth within the Solana ecosystem
+          </Typography>
+        </React.Fragment>,
+        <Typography>200 $GRAPE</Typography>,
+        <Typography>500,000 $GRAPE</Typography>,
+        <Typography>1,500,000 $GRAPE</Typography>,
+        <Typography>100%</Typography>,
+        <Typography>20x</Typography>,
+        <Typography>
+          <CheckIcon />
+        </Typography>,
+        <Typography>
+          <CheckIcon />
+        </Typography>,
+      ],
+    },
+  ],
 };
-const Template: ComponentStory<typeof TableComparison> = () => (
-  <Container>
-    <Grid container spacing={2} sx={{ mx: 'auto' }}>
-      <Grid item xs={12} md={4}>
-        <TableComparison {...data.gibbon} />
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <TableComparison {...data.greatApe} />
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <TableComparison {...data.gorilla} />
-      </Grid>
-    </Grid>
-  </Container>
-);
+
+const Template: ComponentStory<typeof TableComparison> = () => <TableComparison {...data} />;
 
 export const Default = Template.bind({});
--- src/components/TableComparison/index.tsx
+++ src/components/TableComparison/index.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import { Paper, Typography, Grid } from '@mui/material';
+import { Container, Grid, Box, Paper, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
-interface TableComparisonProps {
+interface TableComparisonItemProps {
   className: string;
   classAlias: string;
   minToken: string;
   minTokenLP: string;
-  value?: any;
+  values: (JSX.Element | string)[];
+}
+
+interface TableComparisonProps {
+  headings: (JSX.Element | string)[];
+  items: TableComparisonItemProps[];
 }
 
 export const TableComparison = (props: TableComparisonProps) => {
-  const { className, classAlias, minToken, minTokenLP, value } = props;
-
+  const { headings, items } = props;
   const StyledPaperOuter = styled(Paper)({
     background: 'rgba(255, 255, 255, 0.25)',
-    padding: '1rem',
-    borderRadius: '1.25rem',
+    padding: '0.5rem 1rem',
+    borderRadius: 0,
+    boxShadow: 'none',
+    flex: 1,
+    height: '100%',
   });
 
   const StyledPaperInner = styled(Paper)({
@@ -27,55 +34,176 @@ export const TableComparison = (props: TableComparisonProps) => {
     textAlign: 'center',
     borderRadius: '1.5rem',
     boxShadow: 'none',
-    minHeight: 64,
+    flex: 1,
+    height: '100%',
     '&:last-child': {
       marginBottom: 0,
     },
   });
 
-  let backgroundHeader = 'linear-gradient(90deg, #545769 16.21%, #262834 81.73%)';
-  if (className === 'B') {
-    backgroundHeader = 'linear-gradient(90deg, #D299FF 16.21%, #B4C8FD 81.73%)';
-  } else if (className === 'A') {
-    backgroundHeader = 'linear-gradient(90deg, #EC0FF3 16.21%, #49AEFC 81.04%)';
-  }
+  const TableComparisonItem = (props: TableComparisonItemProps) => {
+    const { className, classAlias, minToken, minTokenLP, values } = props;
 
-  const StyledPaperInnerHeader = styled(StyledPaperInner)({
-    background: backgroundHeader,
-  });
+    let backgroundHeader = 'linear-gradient(90deg, #545769 16.21%, #262834 81.73%)';
+    if (className === 'B') {
+      backgroundHeader = 'linear-gradient(90deg, #D299FF 16.21%, #B4C8FD 81.73%)';
+    } else if (className === 'A') {
+      backgroundHeader = 'linear-gradient(90deg, #EC0FF3 16.21%, #49AEFC 81.04%)';
+    }
+
+    const StyledPaperInnerHeader = styled(StyledPaperInner)({
+      background: backgroundHeader,
+    });
+
+    return (
+      <React.Fragment>
+        <Grid
+          item
+          sx={{
+            order: { xs: 'inherit', lg: 1 },
+          }}
+          xs={12}
+          lg={3}
+        >
+          <StyledPaperOuter
+            sx={{
+              paddingTop: '1rem',
+              borderTopLeftRadius: '1rem',
+              borderTopRightRadius: '1rem',
+              marginLeft: { xs: 0, lg: '1rem' },
+              marginRight: { xs: 0, lg: '1rem' },
+            }}
+          >
+            <StyledPaperInnerHeader>
+              <Grid container sx={{ width: '100%' }}>
+                <Grid item xs={12} sm={6} lg={12} xl={6} sx={{ textAlign: 'left' }}>
+                  <Typography
+                    sx={{ textTransform: 'uppercase' }}
+                  >{`Class ${className}`}</Typography>
+                  <Typography
+                    sx={{ textTransform: 'uppercase', fontSize: '1.5rem' }}
+                    variant="h1"
+                    component="p"
+                  >
+                    {classAlias}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} sm={6} lg={12} xl={6} sx={{ textAlign: 'right' }}>
+                  <Typography
+                    sx={{ textTransform: 'uppercase', fontSize: '1.75rem' }}
+                    variant="h5"
+                    component="p"
+                  >
+                    <img
+                      src="https://via.placeholder.com/30x30?text=X"
+                      alt="Grape Icon"
+                      loading="lazy"
+                      style={{ marginRight: '0.5rem' }}
+                    />
+                    {minToken}
+                  </Typography>
+                  <Typography sx={{ fontSize: '0.8rem' }}>{minTokenLP}</Typography>
+                </Grid>
+              </Grid>
+            </StyledPaperInnerHeader>
+          </StyledPaperOuter>
+        </Grid>
+        {values.map((item, key: number) => (
+          <Grid
+            key={key + 2}
+            item
+            sx={{
+              order: { xs: 'inherit', lg: key + 2 },
+              // Add margin bottom only on Mobile
+              ...(key == values.length - 1 && {
+                marginBottom: { xs: '1rem', lg: 0 },
+              }),
+            }}
+            xs={12}
+            lg={3}
+          >
+            <StyledPaperOuter
+              sx={{
+                flex: 1,
+                marginLeft: { xs: 0, lg: '1rem' },
+                marginRight: { xs: 0, lg: '1rem' },
+                // Add border radius at the bottom
+                ...(key == values.length - 1 && {
+                  paddingBottom: '1rem',
+                  borderBottomLeftRadius: '1rem',
+                  borderBottomRightRadius: '1rem',
+                }),
+              }}
+            >
+              <StyledPaperInner>
+                <Box
+                  sx={{
+                    display: { xs: key != 0 ? 'block' : 'none', lg: 'none' }, // Show heading on top of the value only on Mobile
+                    '& *': {
+                      fontSize: '0.8rem',
+                    },
+                  }}
+                >
+                  {headings[key]}
+                </Box>
+                {item}
+              </StyledPaperInner>
+            </StyledPaperOuter>
+          </Grid>
+        ))}
+      </React.Fragment>
+    );
+  };
 
   return (
-    <StyledPaperOuter>
-      <StyledPaperInnerHeader>
-        <Grid container sx={{ width: '100%' }}>
-          <Grid item xs={12} sm={6} sx={{ textAlign: 'left' }}>
-            <Typography sx={{ textTransform: 'uppercase' }}>{`Class ${className}`}</Typography>
-            <Typography sx={{ textTransform: 'uppercase', fontSize: '1.5rem' }} variant="h1">
-              {classAlias}
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={6} sx={{ textAlign: 'right' }}>
-            <Typography sx={{ textTransform: 'uppercase', fontSize: '2rem' }} variant="h5">
-              <img
-                src="https://via.placeholder.com/30x30?text=X"
-                alt="Grape Icon"
-                loading="lazy"
-                style={{ marginRight: '0.5rem' }}
-              />
-              {minToken}
-            </Typography>
-            <Typography sx={{ fontSize: '0.8rem' }}>{`(${minTokenLP} Raydium LP)`}</Typography>
-          </Grid>
+    <Container maxWidth="xl">
+      <Grid container>
+        <Grid
+          item
+          sx={{
+            paddingTop: '1rem',
+            order: { xs: 'inherit', lg: 1 },
+          }}
+          xs={12}
+          lg={3}
+        >
+          <img
+            src="https://via.placeholder.com/280x100?text=X"
+            alt="Grape Logo"
+            loading="lazy"
+            style={{ width: '100%', maxWidth: '140px' }}
+          />
         </Grid>
-      </StyledPaperInnerHeader>
-      <StyledPaperInner sx={{ minHeight: '220px' + '!important' }}>{value[0]}</StyledPaperInner>
-      <StyledPaperInner>{value[1]}</StyledPaperInner>
-      <StyledPaperInner>{value[2]}</StyledPaperInner>
-      <StyledPaperInner>{value[3]}</StyledPaperInner>
-      <StyledPaperInner>{value[4]}</StyledPaperInner>
-      <StyledPaperInner>{value[5]}</StyledPaperInner>
-      <StyledPaperInner>{value[6]}</StyledPaperInner>
-      <StyledPaperInner>{value[7]}</StyledPaperInner>
-    </StyledPaperOuter>
+
+        {headings.map((item, key: number) => (
+          <Grid
+            item
+            key={key + 2}
+            sx={{
+              padding: '1rem 0',
+              order: { xs: 'inherit', lg: key + 2 },
+              display: { xs: key == 0 ? 'inherit' : 'none', lg: 'flex' },
+              justifyContent: 'flex-start',
+              alignItems: 'flex-end',
+              borderBottom: '1px solid #fff',
+              ...(key == headings.length - 1 && {
+                borderBottom: 'none',
+              }),
+              ...(key == 0 && {
+                borderBottom: 'none',
+              }),
+            }}
+            xs={12}
+            lg={3}
+          >
+            {item}
+          </Grid>
+        ))}
+
+        {items.map((item, key: number) => (
+          <TableComparisonItem key={key} {...item} />
+        ))}
+      </Grid>
+    </Container>
   );
 };
